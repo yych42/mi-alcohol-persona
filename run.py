6a575ce460197993d92a58d38f2d5607c48f3dc6
Yaoyu Chen <43839115+yych42@users.noreply.github.com>
2024-11-08 14:04:29 +0100

    refactor

--- run.py
+++ run.py
@@ -1,12 +1,14 @@
 import os
+from dotenv import load_dotenv
+
 from distilabel.llms import OpenAILLM
 from distilabel.pipeline import Pipeline
-from distilabel.steps import LoadDataFromHub
-from dotenv import load_dotenv
+from distilabel.steps import PushToHub, KeepColumns
+
+from steps.load_data import LoadDataFromHub
 from steps.background_sketch import BackgroundSketch
 from steps.profile import ProfileGeneration
-
-from steps.keep import KeepColumns
+from steps.save_data import PushToHub
 
 load_dotenv()
 
@@ -19,10 +21,12 @@ with Pipeline(
         repo_id="ychen/diverse-persona-10k-no-minors",
         split="train",
         output_mappings={"persona": "persona"},
+        num_examples=1000,
+        shuffle=True,
     )
 
     llm = OpenAILLM(
-        model="openpipe:alcohol-persona-pipe",
+        model="anthropic:claude-3-5-sonnet-20241022",
         api_key=os.getenv("OPENPIPE_API_KEY"),
         base_url="https://api.openpipe.ai/api/v1",
         generation_kwargs={
@@ -43,24 +47,19 @@ with Pipeline(
         input_batch_size=5,
     )
 
-    keep_columns = KeepColumns(
-        columns=["persona", "background_sketch", "profile"], dropna=True
+    keep_columns = KeepColumns(columns=["persona", "background_sketch", "profile"])
+
+    push_to_hub = PushToHub(
+        repo_id="ychen/mi-alcohol-persona-1k-claude",
+        private=True,
+        token="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",
+        dropna=True,
     )
 
-    load_dataset >> background_sketch >> alcohol_profile >> keep_columns
+    load_dataset >> background_sketch >> alcohol_profile >> keep_columns >> push_to_hub
 
 
 if __name__ == "__main__":
-    # Create and run the pipeline
     distiset = pipeline.run(
-        use_cache=False,
-    )
-
-    # Push to hub
-    distiset.push_to_hub(
-        "ychen/mi-alcohol-persona-dev",
-        private=True,
-        token="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",
-        include_script=False,
-        generate_card=False,
+        use_cache=True,
     )
